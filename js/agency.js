f1ad38b6e47b226aa290b2c8cdb97b7c2d79c6d8
sergio <sbpinilla@hotmail.com>
2017-12-29 16:27:58 -0500

    Commit inicial

---
+++ js/agency.js
@@ -0,0 +1,96 @@
+(function ($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 54)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function () {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 54
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function () {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  // Hide navbar when modals trigger
+  $('.portfolio-modal').on('show.bs.modal', function (e) {
+    $(".navbar").addClass("d-none");
+  })
+  $('.portfolio-modal').on('hidden.bs.modal', function (e) {
+    $(".navbar").removeClass("d-none");
+  })
+
+  // Show more
+  $(document).ready(function () {
+    // Configure/customize these variables.
+    var showChar = 70;  // How many characters are shown by default
+    var ellipsestext = "";
+    var moretext = "<i class=\"fa fa-caret-right\" aria-hidden=\"true\"></i>";
+    var lesstext = "<i class=\"fa fa-caret-left\" aria-hidden=\"true\"></i>";
+
+
+    $('.more').each(function () {
+      var content = $(this).html();
+
+      if (content.length > showChar) {
+
+        var c = content.substr(0, showChar);
+        var h = content.substr(showChar, content.length - showChar);
+
+        var html = c + '<span class="moreellipses">' + ellipsestext + '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+
+        $(this).html(html);
+      }
+
+    });
+
+    $(".morelink").click(function () {
+      if ($(this).hasClass("less")) {
+        $(this).removeClass("less");
+        $(this).html(moretext);
+      } else {
+        $(this).addClass("less");
+        $(this).html(lesstext);
+      }
+      $(this).parent().prev().toggle();
+      $(this).prev().toggle();
+      return false;
+    });
+  });
+
+  // Animations MDB
+  $('.navbar-brand').hover(
+    function () { $(this).addClass('animated pulse') },
+    function () { $(this).removeClass('animated pulse') }
+  );
+
+  new WOW().init();
+
+})(jQuery); // End of use strict
